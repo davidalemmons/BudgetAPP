9ae7e8ba1f2d4495c3d5e2a3e1d2766a91809d00
davidalemmons <109745379+davidalemmons@users.noreply.github.com>
2023-12-09 11:36:35 -0500

    Update_1
    updates to functionality as well as the GUI to make the program more user friendly

--- BudgetBuddy.py
+++ BudgetBuddy.py
@@ -1,121 +1,259 @@
+import os
 import tkinter as tk
-from tkinter import ttk, messagebox
+from tkinter import ttk, filedialog, messagebox
+from tkinter import ttk, PhotoImage
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+green_color = "#299617"  # A shade of green
+blue_color = "#6c8aa6"   # A shade of blue
+grey_color = "#a6a6a6"
+white_color = "#Ffffff"
+
+# Create the main application window
+root = tk.Tk()
+root.title("BudgetBuddy")
+root.geometry("1200x800")
+
+# Configure root window's grid
+root.grid_rowconfigure(0, weight=1)
+root.grid_columnconfigure(0, weight=1)
+
+# Create a canvas and a scrollbar
+canvas = tk.Canvas(root)
+scrollbar = ttk.Scrollbar(root, orient="vertical", command=canvas.yview)
+scrollable_frame = ttk.Frame(canvas,style="TFrame")
 
 
-class NavigationBar(tk.Frame):
-    def __init__(self, master, frame_classes):
-        super().__init__(master)
-        self.frame_classes = frame_classes
-        self.create_widgets()
+# Configure the canvas
+canvas.configure(yscrollcommand=scrollbar.set)
+canvas.bind('<Configure>', lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
 
-    def create_widgets(self):
-        for frame_class in self.frame_classes:
-            frame_name = frame_class.__name__
-            button = ttk.Button(self, text=frame_name, command=lambda f=frame_class: self.master.switch_frame(f))
-            button.pack(side='left')
+# Add the frame to the canvas
+canvas.create_window((0, 0), window=scrollable_frame, anchor="nw")
 
-class BudgetApp(tk.Tk):
-    
-    def __init__(self):
-        super().__init__()
-        self.title('BudgetBuddy')
-        self.geometry('600x400')
-        self.style = ttk.Style(self)
-        self.style.theme_use('clam')
-        self.style.configure('TFrame', background='#a4b0be')
-        self.style.configure('TButton', background='#2f3542', foreground='#ffffff')
-        self.style.configure('TLabel', background='#a4b0be', foreground='#2ed573')
-        self.switch_frame(InputFrame)
+# Layout the canvas and scrollbar in the root window
+canvas.grid(row=0, column=0, sticky="nsew")
+scrollbar.grid(row=0, column=1, sticky="ns")
 
-    def switch_frame(self, frame_class):
-        new_frame = frame_class(self)
-        if hasattr(self, 'current_frame'):
-            self.current_frame.destroy()
-        self.current_frame = new_frame
-        self.current_frame.pack(fill='both', expand=True)
+# Lists to store expenses and incomes
+expenses = []
+incomes = []
 
-class InputFrame(tk.Frame):
-    def __init__(self, master):
-        super().__init__(master)
-        self.inputs = {'income': [], 'expenses': []}
-        self.create_widgets()
+# Function to add an expense
+def add_expense():
+    expense_amount = expense_amount_entry.get()
+    expense_label = expense_label_entry.get()
+    expenses.append((expense_label, expense_amount))
+    expense_label_entry.delete(0, tk.END)
+    expense_amount_entry.delete(0, tk.END)
+    update_lists()
+    update_graph()
 
-    def create_widgets(self):
-        ttk.Label(self, text='Income').grid(row=0, column=0, sticky='w')
-        self.add_income_button = ttk.Button(self, text='+ Add Income', command=self.add_income_field)
-        self.add_income_button.grid(row=0, column=1)
+# Function to add an income
+def add_income():
+    income_amount = income_amount_entry.get()
+    income_label = income_label_entry.get()
+    incomes.append((income_label, income_amount))
+    income_label_entry.delete(0, tk.END)
+    income_amount_entry.delete(0, tk.END)
+    update_lists()
+    update_graph()
 
-        ttk.Label(self, text='Expenses').grid(row=1, column=0, sticky='w')
-        self.add_expense_button = ttk.Button(self, text='+ Add Expense', command=self.add_expense_field)
-        self.add_expense_button.grid(row=1, column=1)
+# Function to remove selected items
+def remove_selected():
+    for idx in reversed(expenses_listbox.curselection()):
+        expenses.pop(idx)
+    for idx in reversed(incomes_listbox.curselection()):
+        incomes.pop(idx)
+    update_lists()
+    update_graph()
 
-        self.submit_button = ttk.Button(self, text='Submit', command=self.submit_data)
-        self.submit_button.grid(row=2, column=1, pady=10)
+# Function to update the displayed lists
+def update_lists():
+    expenses_listbox.delete(0, tk.END)
+    incomes_listbox.delete(0, tk.END)
+    for label, amount in expenses:
+        expenses_listbox.insert(tk.END, f"{label}: {amount}")
+    for label, amount in incomes:
+        incomes_listbox.insert(tk.END, f"{label}: {amount}")
 
-    def add_income_field(self):
-        row = len(self.inputs['income']) + 2
-        entry = ttk.Entry(self)
-        entry.grid(row=row, column=0, sticky='ew')
-        self.inputs['income'].append(entry)
+def calculate_total_income():
+    return sum(float(amount) for label, amount in incomes)
 
-    def add_expense_field(self):
-        row = len(self.inputs['expenses']) + 3 + len(self.inputs['income'])
-        entry = ttk.Entry(self)
-        entry.grid(row=row, column=0, sticky='ew')
-        self.inputs['expenses'].append(entry)
+def calculate_total_expenses():
+    return sum(float(amount) for label, amount in expenses)
 
-    def submit_data(self):
-        income_data = [entry.get() for entry in self.inputs['income']]
-        expenses_data = [entry.get() for entry in self.inputs['expenses']]
-        messagebox.showinfo('Submitted', 'Your data has been submitted.')
-        print('Income:', income_data)
-        print('Expenses:', expenses_data)
+# Global variables for the figure and the canvas
+global fig, ax, canvas_widget
 
-class BudgetFrame(tk.Frame):
-    def __init__(self, master):
-        super().__init__(master)
-        self.configure(bg='#80cbc4')
-        self.create_widgets()
+# Create the figure for the plots
+fig, ax = plt.subplots(1, 2, figsize=(10, 5))
 
-    def create_widgets(self):
-        ttk.Label(self, text="Budget Summary", font=('Helvetica', 14, "bold"), background='#80cbc4', foreground='#004d40').pack(pady=10)
+# Create a canvas for the figure (to be packed later in a new window)
+canvas_widget = FigureCanvasTkAgg(fig)
 
-        self.total_income_label = ttk.Label(self, text="Total Income: $0.00", background='#80cbc4', foreground='#004d40')
-        self.total_income_label.pack(pady=5)
-        
-        self.total_expenses_label = ttk.Label(self, text="Total Expenses: $0.00", background='#80cbc4', foreground='#004d40')
-        self.total_expenses_label.pack(pady=5)
-        
-        self.budget_label = ttk.Label(self, text="Budget: $0.00", background='#80cbc4', foreground='#004d40')
-        self.budget_label.pack(pady=5)
+def update_graph():
+    global canvas_widget
 
-    def update_budget_summary(self, total_income, total_expenses, budget):
-        self.total_income_label.config(text=f"Total Income: ${total_income:.2f}")
-        self.total_expenses_label.config(text=f"Total Expenses: ${total_expenses:.2f}")
-        self.budget_label.config(text=f"Budget: ${budget:.2f}")
+    # Clear the previous figures
+    ax[0].clear()
+    ax[1].clear()
 
-class GraphFrame(tk.Frame):
-    def __init__(self, master):
-        super().__init__(master)
-        self.configure(bg='white')
-        self.create_widgets()
+    # Data for expense chart
+    expense_labels = [label for label, amount in expenses]
+    expense_amounts = [float(amount) for label, amount in expenses]
 
-    def create_widgets(self):
-        self.figure, self.ax = plt.subplots(figsize=(6, 4))
-        self.ax.set_title("Budget Graph")
-        
-        self.canvas = FigureCanvasTkAgg(self.figure, master=self)
-        self.canvas_widget = self.canvas.get_tk_widget()
-        self.canvas_widget.pack()
+    # Data for income chart
+    total_income = calculate_total_income()
+    total_expenses = calculate_total_expenses()
+    income_unused = max(total_income - total_expenses, 0)
 
-    def update_budget_graph(self, x_values, y_values):
-        self.ax.clear()
-        self.ax.plot(x_values, y_values, marker='o')
-        self.ax.set_xlabel('Month')
-        self.ax.set_ylabel('Budget ($)')
-        self.ax.set_title("Budget Graph")
-        self.canvas.draw()
+    # Plotting the expense chart
+    if expense_amounts:
+        ax[0].pie(expense_amounts, labels=expense_labels, autopct='%1.1f%%', startangle=140)
+        ax[0].set_title('Expense Distribution')
+    else:
+        ax[0].text(0.5, 0.5, 'No Expense Data', horizontalalignment='center', verticalalignment='center')
 
-if __name__ == "__main__":
-    app = BudgetApp()
-    app.mainloop()
+    # Plotting the income chart
+    income_labels = ['Income Used', 'Income Unused']
+    income_amounts = [total_expenses, income_unused]
+    ax[1].pie(income_amounts, labels=income_labels, autopct='%1.1f%%', startangle=140, colors=['red', 'green'])
+    ax[1].set_title('Income Usage')
+
+    # Draw the updated plots
+    plot_graphs()
+    canvas_widget.draw()
+
+def open_graph_window():
+    global fig, ax
+
+    graph_window = tk.Toplevel(root)
+    graph_window.title("Budget Graphs")
+    graph_window.geometry("600x600")
+
+    new_canvas_widget = FigureCanvasTkAgg(fig, master=graph_window)
+    new_canvas_widget_widget = new_canvas_widget.get_tk_widget()
+    new_canvas_widget_widget.pack(fill=tk.BOTH, expand=True)
+
+    print_button = ttk.Button(graph_window, text="Print Graph", command=print_graph)
+    print_button.pack(pady=10)
+
+    plot_graphs()
+    new_canvas_widget.draw()
+
+def print_graph():
+    # Save the figure to a temporary file
+    temp_file = "temp_graph.png"
+    fig.savefig(temp_file)
+
+    # Implement platform-specific print command
+    if os.name == 'nt':  # Windows
+        os.startfile(temp_file, "print")
+    else:  # macOS, Linux, and others
+        # Command for macOS: open -a Preview.app <filename>
+        # Command for Linux: lpr <filename>
+        # Adjust the command according to your requirement
+        print_command = f"lpr {temp_file}"
+        os.system(print_command)
+
+    messagebox.showinfo("Print Graph", "Graph sent to the printer.")
+
+def plot_graphs():
+    global fig, ax, expenses, incomes
+
+    # Clear the previous figures
+    ax[0].clear()
+    ax[1].clear()
+
+    # Data for expense chart
+    expense_labels = [label for label, amount in expenses]
+    expense_amounts = [float(amount) for label, amount in expenses]
+
+    # Data for income chart
+    total_income = calculate_total_income()
+    total_expenses = calculate_total_expenses()
+    income_unused = max(total_income - total_expenses, 0)
+
+    # Plotting the expense chart
+    if expense_amounts:
+        ax[0].pie(expense_amounts, labels=expense_labels, autopct='%1.1f%%', startangle=140)
+        ax[0].set_title('Expense Distribution')
+    else:
+        ax[0].text(0.5, 0.5, 'No Expense Data', horizontalalignment='center', verticalalignment='center')
+
+    # Plotting the income chart
+    income_labels = ['Income Used', 'Income Unused']
+    income_amounts = [total_expenses, income_unused]
+    ax[1].pie(income_amounts, labels=income_labels, autopct='%1.1f%%', startangle=140, colors=['red', 'green'])
+    ax[1].set_title('Income Usage')
+
+logo_image = PhotoImage(file='C:\\Users\\david\\OneDrive\\Desktop\\BudgetBuddy\\BudgetApp\\BBLogo.png')
+
+logo_label = ttk.Label(scrollable_frame, image=logo_image)
+logo_label.image = logo_image  # Keep a reference to avoid garbage collection
+logo_label.grid(row=0, column=2, padx=50, pady=10)  # Adjust grid parameters as needed
+
+# Labels for expense label and amount entries
+
+expense_label_label = ttk.Label(scrollable_frame, text="Expense Label:")
+expense_label_label.grid(row=1, column=1, padx=10, pady=5, sticky="ew")
+
+expense_amount_label = ttk.Label(scrollable_frame, text="Expense Amount:")
+expense_amount_label.grid(row=2, column=1, padx=10, pady=5, sticky="ew")
+
+# Entry fields for expense labels and amounts
+expense_label_entry = ttk.Entry(scrollable_frame)
+expense_label_entry.grid(row=1, column=2, padx=10, pady=5, sticky="ew")
+
+expense_amount_entry = ttk.Entry(scrollable_frame)
+expense_amount_entry.grid(row=2, column=2, padx=10, pady=5, sticky="ew")
+
+# Labels for income label and amount entries
+income_label_label = ttk.Label(scrollable_frame, text="Income Label:")
+income_label_label.grid(row=3, column=1, padx=10, pady=5, sticky="ew")
+
+income_amount_label = ttk.Label(scrollable_frame, text="Income Amount:")
+income_amount_label.grid(row=4, column=1, padx=10, pady=5, sticky="ew")
+
+# Entry fields for income labels and amounts
+income_label_entry = ttk.Entry(scrollable_frame)
+income_label_entry.grid(row=3, column=2, padx=10, pady=5, sticky="ew")
+
+income_amount_entry = ttk.Entry(scrollable_frame)
+income_amount_entry.grid(row=4, column=2, padx=10, pady=5, sticky="ew")
+
+style = ttk.Style()
+style.configure("TButton", background=green_color)
+style.configure("TFrame", background=green_color)
+# Buttons to add expenses, incomes, and remove selected items
+expense_button = ttk.Button(scrollable_frame, text="Add Expense", command=add_expense)
+income_button = ttk.Button(scrollable_frame, text="Add Income", command=add_income)
+remove_button = ttk.Button(scrollable_frame, text="Remove Selected", command=remove_selected)
+expense_button.grid(row=5, column=1, columnspan=2, pady=10, sticky="ew")
+expense_button.configure(style="TButton")
+income_button.grid(row=6, column=1, columnspan=2, pady=10, sticky="ew")
+remove_button.grid(row=7, column=1, columnspan=2, pady=10, sticky="ew")
+
+# Listboxes to display expenses and incomes
+expenses_listbox = tk.Listbox(scrollable_frame, height=10, width=40, selectmode=tk.MULTIPLE)
+incomes_listbox = tk.Listbox(scrollable_frame, height=10, width=40, selectmode=tk.MULTIPLE)
+expenses_listbox.grid(row=8, column=1, columnspan=2, pady=10, sticky="ew")
+incomes_listbox.grid(row=9, column=1, columnspan=2, pady=10, sticky="ew")
+expenses_listbox.configure(bg=grey_color, fg=white_color)
+incomes_listbox.configure(bg=grey_color, fg=white_color)
+
+# Labels for displaying totals
+total_income_label = ttk.Label(scrollable_frame, text="Total Income: $0")
+total_income_label.grid(row=11, column=1, columnspan=2, sticky="ew")
+
+total_expenses_label = ttk.Label(scrollable_frame, text="Total Expenses: $0")
+total_expenses_label.grid(row=12, column=1, columnspan=2, sticky="ew")
+
+# Button to open the graph window
+graph_button = ttk.Button(scrollable_frame, text="Budget Graph", command=open_graph_window)
+graph_button.grid(row=13, column=1, columnspan=2, pady=10, sticky="ew")
+
+# Run the application
+root.mainloop()
